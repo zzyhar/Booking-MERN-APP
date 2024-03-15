4c38afef3d76d9491a49e710075e19b39702907f
Zyhar Zakhar <93952085+zzyhar@users.noreply.github.com>
2024-03-15 03:20:44 -0400

    Creating User Auth API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose'
 import userRoutes from './routes/users'
+import authRoutes from './routes/auth'
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_CONECTION_STRING as string)
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes)
 
 
